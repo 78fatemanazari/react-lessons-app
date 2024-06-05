353e7fb4fdf45a4ecf3bb6b37a7145a11397eb39
fatemanazari <fatemanazari78@gmail.com>
2024-06-05 11:06:04 +0330

    feat: Split add course component from app component and make a new one

--- src/Course.js
+++ src/Course.js
@@ -1,12 +1,60 @@
-export const Course = (props) => {
+import React from 'react';
+import { useState } from 'react';
 
+export const Course = () => {
+  const [courseList, setCourseList] = useState([]);
+  const [newCourse, setNewCourse] = useState("");
+
+    const handleChange = (event) => {
+      setNewCourse(event.target.value);
+    }
+
+    const addCourse = () => {
+      const course = {
+        id: courseList.length === 0 ? 1 : courseList[courseList.length - 1].id + 1,
+        courseName: newCourse,
+        isCompleted: false
+      }
+      setCourseList([...courseList, course]);
+      console.log(courseList);
+    }
+
+    const deleteCourse = (courseId) => {
+      setCourseList(courseList.filter((course) =>
+        courseId !== course.id
+    ))
+    }
+
+    const completeCourse = (courseId) =>  {
+      const newCourseList = courseList.map((course) =>{
+        if (course.id === courseId) 
+          return {...course, isCompleted : !course.isCompleted}
+        else 
+          return course
+      })
+      setCourseList(newCourseList)
+    }
   return (
-     <div>
-        <h1 style={{backgroundColor: props.course.isCompleted ? "green" : "White"}}>
-          {props.course.courseName}
-        </h1>
-        <button onClick={ () => props.deleteCourse(props.course.id)}>delete</button>
-        <button onClick={ () => props.completeCourse(props.course.id)}>complete</button>
-     </div> 
+    <div className="App">
+      <div className='add-course'>
+        <input type='text' onChange={handleChange}></input>
+        <button onClick={addCourse}>Add Course</button>
+      </div>
+      <div className='list'>
+          {courseList.map((course) => {
+            return (
+              <div>
+                <h1 style={{backgroundColor: course.isCompleted ? "green" : "White"}}>
+                  {course.courseName}
+                </h1>
+                <button onClick={ () => deleteCourse(course.id)}>delete</button>
+                <button onClick={ () => completeCourse(course.id)}>complete</button>
+               </div>
+            ) 
+          })}
+      </div>
+    </div>
+      
+
   )
 }
