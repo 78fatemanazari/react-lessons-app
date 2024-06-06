2c4494629fb7866d6902234ebb472a7299ec61b3
fatemanazari <fatemanazari78@gmail.com>
2024-06-06 08:55:40 +0330

    feat: Add useNavigate from react router dom

--- src/pages/Excuse.js
+++ src/pages/Excuse.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 
 export const Excuse = () => {
+  let navigate = useNavigate();
   const [fetchExcuse, setFetchExcuse] = useState("");
   const fetchAllExcuse = (excuse) => {
     Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
@@ -12,12 +14,15 @@ export const Excuse = () => {
   };
 
   return (
-    <div className="App">
-      <button onClick={()=>fetchAllExcuse("party")}>Party</button>
-      <button onClick={()=>fetchAllExcuse("family")}>Family</button>
-      <button onClick={()=>fetchAllExcuse("office")}>Office</button>
-      <h1>{fetchExcuse}</h1>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <button onClick={()=>fetchAllExcuse("party")}>Party</button>
+        <button onClick={()=>fetchAllExcuse("family")}>Family</button>
+        <button onClick={()=>fetchAllExcuse("office")}>Office</button>
+        <h1>{fetchExcuse}</h1>
+      </div>
+      <button onClick={() => (navigate('/course'))}>Go to course page</button>
+    </React.Fragment>
   );
 };
 
