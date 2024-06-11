78bfb4734cbb8cb5ae1f295ae169836585ce4886
fatemanazari <fatemanazari78@gmail.com>
2024-06-11 10:45:11 +0330

    feat: Make some comments

--- src/pages/Excuse.js
+++ src/pages/Excuse.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+//React Query Import
 import { useQuery } from "react-query";
 import Axios from "axios";
 
 export const Excuse = () => {
   let navigate = useNavigate();
   const [fetchExcuse, setFetchExcuse] = useState("");
+  //React query
   const {data, isLoading, isError, refetch} = useQuery(["excuse"], ()=>{
     return Axios.get("https://excuser-three.vercel.app/v1/excuse").then((res) => res.data)
     })
 
     if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error fetching data</p>;
-  
+  /////
     const fetchAllExcuse = (excuse) => {
     Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
       console.log(res.data);
@@ -30,6 +32,7 @@ export const Excuse = () => {
         <h1>{fetchExcuse}</h1>
       </div>
       <button onClick={() => (navigate('/course'))}>Go to course page</button>
+      {/* React Query */}
       <h1>{data[0].excuse}</h1>
       <button onClick={refetch}>Refresh</button>
     </React.Fragment>
